0b0296b93b0c09bb7607967bb78118f86d56f821
AmuletxHeart <amuletxheart@gmail.com>
2014-12-09 14:37:00 +0800

    basic framework

--- mobile/src/main/java/com/amuletxheart/cameraderie/MainActivity.java
+++ mobile/src/main/java/com/amuletxheart/cameraderie/MainActivity.java
@@ -1,19 +1,30 @@
 package com.amuletxheart.cameraderie;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = MainActivity.class.toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "fonts/asenine.ttf");
+        TextView myTextView = (TextView)findViewById(R.id. textViewTitle);
+        myTextView.setTypeface(myTypeface);
+
+        initClickEvents();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +46,23 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initClickEvents(){
+        LinearLayout linearLayoutCamera = (LinearLayout)findViewById(R.id.linearLayoutCamera);
+        linearLayoutCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Clicked on camera button.");
+            }
+        });
+
+        LinearLayout linearLayoutGallery = (LinearLayout)findViewById(R.id.linearLayoutGallery);
+        linearLayoutGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Clicked on gallery button.");
+            }
+        });
     }
 }
